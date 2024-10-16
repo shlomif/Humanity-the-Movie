db0adc8698470f54261636e7831a3c4ffc999a55
Shlomi Fish <shlomif@shlomifish.org>
2024-10-16 07:40:18 +0300

    Fix tidyall tests.
    See:
    
    https://metacpan.org/release/Code-TidyAll .

--- humanity/screenplay/scripts/prepare-epub.pl
+++ humanity/screenplay/scripts/prepare-epub.pl
@@ -5,48 +5,48 @@ use warnings;
 
 use utf8;
 
-use Shlomif::Screenplays::EPUB;
+use Shlomif::Screenplays::EPUB ();
 
 my $obj = Shlomif::Screenplays::EPUB->new;
 $obj->run;
 
-my $gfx = $obj->gfx;
+my $gfx      = $obj->gfx;
 my $filename = $obj->filename;
 
-foreach my $part ($filename =~ /hebrew/i ? 1 : 0)
+foreach my $part ( $filename =~ /hebrew/i ? 1 : 0 )
 {
     my $epub_basename = ( $filename =~ s/\.screenplay-text.xhtml\z//r );
     $obj->epub_basename($epub_basename);
 
     $obj->output_json(
         {
-            data =>
-            {
+            data => {
                 filename => $epub_basename,
-                title => "Humanity - The Movie" . ($part ? " - Hebrew Translation" : ""),
-                authors =>
-                [
+                title    => "Humanity - The Movie"
+                    . ( $part ? " - Hebrew Translation" : "" ),
+                authors => [
                     {
                         name => "Shlomi Fish",
                         sort => "Fish, Shlomi",
                     },
                 ],
-                contributors =>
-                [
+                contributors => [
                     {
                         name => "Shlomi Fish",
                         role => "oth",
                     },
                 ],
-                cover => "images/$gfx",
-                rights => "Creative Commons Attribution ShareAlike Unported (CC-by-sa-3.0)",
+                cover  => "images/$gfx",
+                rights =>
+"Creative Commons Attribution ShareAlike Unported (CC-by-sa-3.0)",
                 publisher => 'http://www.shlomifish.org/',
-                language => 'en-GB',
-                subjects => [ 'FICTION/Horror', 'FICTION/Humorous', 'FICTION/Mashups', ],
-                identifier =>
-                {
+                language  => 'en-GB',
+                subjects  => [
+                    'FICTION/Horror', 'FICTION/Humorous', 'FICTION/Mashups',
+                ],
+                identifier => {
                     scheme => 'URL',
-                    value => 'http://www.shlomifish.org/humour/humanity/',
+                    value  => 'http://www.shlomifish.org/humour/humanity/',
                 },
             },
         },
